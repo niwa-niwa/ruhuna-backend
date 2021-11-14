aa181dacc967bce7e4e71328a47a5aa1c19cb35d
niwa-niwa <urban.hang274@gmail.com>
2021-11-14 23:58:15 +0900

    implemented paths for user

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -1,10 +1,6 @@
 import { Request, Response } from "express";
 import { FirebaseAdmin } from "../../lib/FirebaseAdmin";
-
-export const index = async (req: Request, res: Response) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("api version 1+ : The Express with Node.js right now");
-};
+import { PrismaClient } from ".prisma/client";
 
 export const auth = async (req: Request, res: Response) => {
   const idToken: string | undefined = req.header("Authorization");
@@ -20,4 +16,46 @@ export const auth = async (req: Request, res: Response) => {
   }
 };
 
-export default { index, auth };
+// todo : implement what get a User by id
+export const getUser = async (req: Request, res: Response) => {
+  const id: string = "1";
+  const prisma: PrismaClient = new PrismaClient();
+  const user: object | null = await prisma.user.findUnique({ where: { id } });
+
+  if (!user) {
+    res.status(404).json({ message: "該当ユーザーはいまん" });
+  }
+
+  res.status(202).json({ user });
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+  const prisma: PrismaClient = new PrismaClient();
+  const allUsers = await prisma.user.findMany();
+  res.status(200).json({ users: allUsers });
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  // todo : implement what create a user
+  // todo : response a token that firebase auth generated
+  res.status(200).json({ message: "create a user" });
+};
+
+export const editUser = async (req: Request, res: Response) => {
+  // todo :implement what edit a user by user id
+  res.status(200).json({ message: "edit a user" });
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  // todo : implement what delete a user by user id
+  res.status(200).json({ message: "delete a user" });
+};
+
+export default {
+  auth,
+  getUsers,
+  getUser,
+  createUser,
+  editUser,
+  deleteUser,
+};
--- src/api/routers/_v1.ts
+++ src/api/routers/_v1.ts
@@ -3,7 +3,11 @@ import user from "../controllers/userController";
 
 const v1: Router = express.Router();
 
-v1.get("/", user.index);
 v1.post("/auth", user.auth);
+v1.get("/users", user.getUsers);
+v1.post("/user", user.getUser);
+v1.post("/user/create", user.createUser);
+v1.put("/user/edit", user.editUser);
+v1.delete("/user/delete", user.deleteUser);
 
 export { v1 };
